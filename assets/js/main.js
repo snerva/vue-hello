628033b43a2dd867d372c6c744c02ce7c65abd90
snerva <selenenerva@live.it>
2022-10-26 16:42:59 +0200

    syntax correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,14 @@
 //stampa a schermo un messaggio all interno di un h1 utilizzando i data
 const {createApp} = Vue
 
-// createApp ({
-//     data() {
-//         return {
-//             message: 'HELLO THERE!',
-//             mexStyle: 'h1',
-//             image: './assets/img/ObiWan.jpg',
-//             imgStyle: 'img'
-//         }
-//     }
-// }).mount('#app')
-
 createApp ({
     data() {
         return {
             message: 'HELLO THERE!',
-            mexStyle: {
-                color: 'gold',
-                fontSize: '60px',
-                margin: '3rem'
-            },
+            mexStyle: 'h1',
             image: './assets/img/ObiWan.jpg',
-            imgStyle: {
-                width: '700px',
-                maxWidth: '100%' 
-            }
-           
+            imgStyle: 'img'
         }
     }
 }).mount('#app')
+
