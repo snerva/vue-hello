4f7294b7244087e45a5ca67c758ba154a99aa4d5
snerva <selenenerva@live.it>
2022-10-26 16:24:22 +0200

    second syntax option

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,32 @@
 //stampa a schermo un messaggio all interno di un h1 utilizzando i data
 const {createApp} = Vue
 
+// createApp ({
+//     data() {
+//         return {
+//             message: 'HELLO THERE!',
+//             mexStyle: 'h1',
+//             image: './assets/img/ObiWan.jpg',
+//             imgStyle: 'img'
+//         }
+//     }
+// }).mount('#app')
+
 createApp ({
     data() {
         return {
             message: 'HELLO THERE!',
-            mexStyle: 'h1',
+            mexStyle: {
+                color: 'gold',
+                fontSize: '60px',
+                margin: '3rem'
+            },
             image: './assets/img/ObiWan.jpg',
-            imgStyle: 'img'
+            imgStyle: {
+                width: '700px',
+                maxWidth: '100%' 
+            }
+           
         }
     }
 }).mount('#app')
-
-
